23344f50d16ebe2201dbec7644d011314e048f9b
kirank <kirank@useready.com>
2021-02-26 05:04:59 -0800

    Remove unwanted files

--- src/TableColumnModule.js
+++
@@ -1,8 +0,0 @@
-
-
-// var tableausheetName = {
-//     sheet1:"Extension Input"
-// };
-
-let datatableColumn = ['MY(Contract Date)','Sales', 'Revenue Budget','Forecast_Amount', 'Category','Row_ID']
-module.exports = {datatableColumn}; 
--- src/constants.js
+++
@@ -1,1 +0,0 @@
-export const rest_server_url = 'https://uracme-waitress.useready.com:9443/tableauextensions/writeback/api/'
